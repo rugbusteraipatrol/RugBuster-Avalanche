e1f08744c6551bbc087ff8d00f13a633914cb96e
rugbusteraipatrol <rug.buster@pm.me>
2026-05-29 12:11:06 +0200

    Add RugBusterScanner Fuji contract

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const privateKey = process.env.PRIVATE_KEY || "";
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    fuji: {
+      url: process.env.FUJI_RPC_URL || "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: privateKey ? [privateKey] : [],
+    },
+  },
+};
